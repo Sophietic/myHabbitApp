e7cca1687c46f6e6b92f2c684ba6881dc79557d6
sophiepa <sophiepa@hotmail.com>
2020-12-10 13:52:10 +0100

    deploy try

--- server/app.js
+++ server/app.js
@@ -13,44 +13,44 @@ const session = require("express-session");
 
 require("./configs/passport.configs");
 // process.env.MONGO_ATLAS_URI || 
-const { MONGO_ATLAS, MONGO_LOCAL, NODE_ENV } = process.env;
+// const { MONGO_ATLAS, MONGO_LOCAL, NODE_ENV } = process.env;
 
-// Function to run database connection
-const connectDb = (mongoUri) =>
-  mongoose
-    .connect(mongoUri, {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    })
-    .then((x) => {
-      console.log(
-        `Connected to Mongo! Database name: "${x.connections[0].name}"`
-      );
-    })
-    .catch((err) => {
-      console.error("Error connecting to mongo", err);
-    });
+// // Function to run database connection
+// const connectDb = (mongoUri) =>
+//   mongoose
+//     .connect(mongoUri, {
+//       useCreateIndex: true,
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify: false,
+//     })
+//     .then((x) => {
+//       console.log(
+//         `Connected to Mongo! Database name: "${x.connections[0].name}"`
+//       );
+//     })
+//     .catch((err) => {
+//       console.error("Error connecting to mongo", err);
+//     });
 
-// Check if development environment is 
-NODE_ENV === "development" ? connectDb(MONGO_LOCAL) : connectDb(MONGO_ATLAS);
+// // Check if development environment is 
+// NODE_ENV === "development" ? connectDb(MONGO_LOCAL) : connectDb(MONGO_ATLAS);
 
-// mongoose
-//   .connect(process.env.MONGO_ATLAS|| "mongodb://localhost/server", {
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//   })
-//   .then((x) => {
-//     console.log(
-//       `Connected to Mongo! Database name: "${x.connections[0].name}"`
-//     );
-//   })
-//   .catch((err) => {
-//     console.error("Error connecting to mongo", err);
-//   });
+mongoose
+  .connect(process.env.MONGO_ATLAS|| "mongodb://localhost/server", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+  });
 
 const app = express();
 
@@ -99,12 +99,16 @@ app.use("/api", router);
 const habits = require("./routes/habits.routes");
 app.use("/api", habits);
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// Setting up environments
+if (process.env.NODE_ENV === "production") {
+  // set ability to get static values from client build folder
+  // static files include all javascript and css files
+  app.use(express.static("client/build"));
 
-  app.get('*', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // get the index.html that will be rendered on the browser
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "../client", "build", "index.html"));
   });
 }
+
+module.exports = app;
