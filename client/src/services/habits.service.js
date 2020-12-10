fa20cc6efb3fc373b692762486271f7ee734c89c
sophiepa <sophiepa@hotmail.com>
2020-12-10 12:47:07 +0100

    changed service responsedata

--- client/src/services/habits.service.js
+++ client/src/services/habits.service.js
@@ -4,18 +4,13 @@ import axios from "axios";
 class HabitService {
   constructor() {
     this.service = axios.create({
-      baseURL: "https://myhabitapp.herokuapp.com/api",
+      baseURL: "https://myhabitapp.herokuapp.com",
       withCredentials: true, // indicates whether or not cross-site Access-Control requests should be made using credentials
     });
   }
 
- getlist = () => {
-    return this.service
-      .get("/explore")
-      .then((response) => response.data);
+  getlist = () => {
+    return this.service.get("/api/explore").then((response) => response);
   };
- 
-
-
 }
 export default HabitService;
