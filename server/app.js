4af7b2fd1afb40ed4f78e0426c7f378bea5f992f
sophiepa <sophiepa@hotmail.com>
2020-12-10 06:47:50 +0100

    deploy heroku settings

--- server/app.js
+++ server/app.js
@@ -13,22 +13,44 @@ const session = require("express-session");
 
 require("./configs/passport.configs");
 // process.env.MONGO_ATLAS_URI || 
+const { MONGO_ATLAS, MONGO_LOCAL, NODE_ENV } = process.env;
 
-mongoose
-  .connect(process.env.MONGO_ATLAS_URI|| "mongodb://localhost/server", {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then((x) => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo", err);
-  });
+// Function to run database connection
+const connectDb = (mongoUri) =>
+  mongoose
+    .connect(mongoUri, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+    .then((x) => {
+      console.log(
+        `Connected to Mongo! Database name: "${x.connections[0].name}"`
+      );
+    })
+    .catch((err) => {
+      console.error("Error connecting to mongo", err);
+    });
+
+// Check if development environment is 
+NODE_ENV === "development" ? connectDb(MONGO_LOCAL) : connectDb(MONGO_ATLAS);
+
+// mongoose
+//   .connect(process.env.MONGO_ATLAS_URI|| "mongodb://localhost/server", {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//   })
+//   .then((x) => {
+//     console.log(
+//       `Connected to Mongo! Database name: "${x.connections[0].name}"`
+//     );
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to mongo", err);
+//   });
 
 const app = express();
 
@@ -77,5 +99,18 @@ const router = require("./routes/auth.routes");
 app.use("/api", router);
 const habits = require("./routes/habits.routes");
 app.use("/api", habits);
+
+// Setting up environments
+if (process.env.NODE_ENV === "production") {
+  // set ability to get static values from client build folder
+  // static files include all javascript and css files
+  app.use(express.static("client/build"));
+
+  // get the index.html that will be rendered on the browser
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "../client", "build", "index.html"));
+  });
+}
+
 
 module.exports = app;
