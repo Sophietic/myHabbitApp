de26dcec61a1db102ef6101c27e3f5d695759dbf
sophiepa <sophiepa@hotmail.com>
2020-12-11 18:53:59 +0100

    heroku

--- server/app.js
+++ server/app.js
@@ -10,31 +10,9 @@ const path = require("path");
 const cors = require("cors");
 const passport = require("passport");
 const session = require("express-session");
+const app = express();
 
 require("./configs/passport.configs");
-// process.env.MONGO_ATLAS_URI || 
-// const { MONGO_ATLAS, MONGO_LOCAL, NODE_ENV } = process.env;
-
-// // Function to run database connection
-// const connectDb = (mongoUri) =>
-//   mongoose
-//     .connect(mongoUri, {
-//       useCreateIndex: true,
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       useFindAndModify: false,
-//     })
-//     .then((x) => {
-//       console.log(
-//         `Connected to Mongo! Database name: "${x.connections[0].name}"`
-//       );
-//     })
-//     .catch((err) => {
-//       console.error("Error connecting to mongo", err);
-//     });
-
-// // Check if development environment is 
-// NODE_ENV === "development" ? connectDb(MONGO_LOCAL) : connectDb(MONGO_ATLAS);
 
 mongoose
   .connect(process.env.MongoDB_URI, {
@@ -52,7 +30,6 @@ mongoose
     console.error("Error connecting to mongo", err);
   });
 
-const app = express();
 
 // Middleware Setup
 app.use(logger("dev"));
@@ -61,7 +38,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://myhabitapp.herokuapp.com"],
+    origin: ["http://localhost:3000"],
     credentials: true,
   })
 );
@@ -89,9 +66,6 @@ app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
-// default value for title local
-app.locals.title = "Express - Generated with IronGenerator";
-
 const index = require("./routes/index");
 app.use("/api", index);
 const router = require("./routes/auth.routes");
@@ -99,16 +73,10 @@ app.use("/api", router);
 const habits = require("./routes/habits.routes");
 app.use("/api", habits);
 
-// Setting up environments
-if (process.env.NODE_ENV === "production") {
-  // set ability to get static values from client build folder
-  // static files include all javascript and css files
-  app.use(express.static("client/build"));
-
-  // get the index.html that will be rendered on the browser
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "../client", "build", "index.html"));
-  });
-}
+app.use('/static', express.static(path.join(__dirname, '../client/build/static')));
+app.use('/', express.static(path.join(__dirname, '../client/build/')));
+app.get('*', function(request, response) {
+  response.sendFile('index.html', {root: path.join(__dirname, '../client/build/')});
+});
 
 module.exports = app;
